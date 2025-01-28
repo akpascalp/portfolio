72851189640f8e5d0141da2d23b7f3829abbd294
Pascal Pham <pham.anhkhoapascal@gmail.com>
2025-01-28 12:09:16 +0100

    Presentation Widget

---
+++ frontend/src/components/dashboard/PresentationWidget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-span-12 lg:col-span-6 xl:col-span-6">
+        <div class="card mb-0">
+            <div class="grid grid-cols-2 gap-4">
+                <img alt="user header" src="/profil.jpg" />
+                <div class="flex flex-col gap-2 mb-4">
+                    <span class="block text-muted-color font-medium">Pascal PHAM</span>
+                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">Ingénieur logiciel Python | Vue.js</div>
+                    <div class="text-muted-color dark:text-surface-400">Développeur freelance, expert en Python (Flask, Django, PyQt, data science), front-end (Vue.js) et bases de données (MongoDB).</div>
+                    <div class="text-surface-900 dark:text-surface-0">
+                        <i class="pi pi-star"></i>
+                        +7 ans d'exp.
+                    </div>
+                    <div>
+                        <div class="text-surface-900 dark:text-surface-0">
+                            <i class="pi pi-github"></i>
+                            <a href="https://github.com/akpascalp/" target="_blank"> akpascalp</a>
+                        </div>
+                        <div class="text-surface-900 dark:text-surface-0">
+                            <i class="pi pi-linkedin"></i>
+                            <a href="https://www.linkedin.com/in/anhkhoa-pascal-pham/" target="_blank"> Pascal Anh-Khoa PHAM</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,14 +1,11 @@
 <script setup>
-import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
-import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
-import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
-import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
-import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+import PresentationWidget from '@/components/dashboard/PresentationWidget.vue';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
-        <StatsWidget />
+        <PresentationWidget />
+        <!-- <StatsWidget />
 
         <div class="col-span-12 xl:col-span-6">
             <RecentSalesWidget />
@@ -17,6 +14,6 @@ import StatsWidget from '@/components/dashboard/StatsWidget.vue';
         <div class="col-span-12 xl:col-span-6">
             <RevenueStreamWidget />
             <NotificationsWidget />
-        </div>
+        </div> -->
     </div>
 </template>
