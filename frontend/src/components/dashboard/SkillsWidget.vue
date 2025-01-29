f86a7cbb43c3788c8879c4dce661b8c98738cda0
Pascal Pham <pham.anhkhoapascal@gmail.com>
2025-01-29 15:31:26 +0100

    Skills Widget

---
+++ frontend/src/components/dashboard/SkillsWidget.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { onMounted } from 'vue';
+import SpriteText from 'https://esm.sh/three-spritetext';
+
+const technologies = {
+    Python: {
+        GUI: ['PyQt'],
+        Web: ['Flask', 'Django', 'FastAPI'],
+        'Data Science': ['Keras', 'Tensorflow']
+    },
+    JavaScript: {
+        frontend: ['Vue.js'],
+        '3D': ['Three.js']
+    },
+    Database: {
+        SQL: ['MySQL'],
+        NoSQL: ['MongoDB']
+    },
+    PHP: {
+        backend: ['Laravel']
+    },
+    'REST API': {
+        tools: ['Postman']
+    },
+    'CI/CD': ['Docker', 'Git', 'Jenkins']
+};
+
+const createGraph = () => {
+    const graphData = {
+        nodes: [{ id: '', group: -1 }],
+        links: []
+    };
+
+    const addNodesAndLinks = (parent, obj, group) => {
+        Object.keys(obj).forEach((key, index) => {
+            const currentGroup = group === 0 ? index - 1 : group;
+            graphData.nodes.push({ id: key, group: currentGroup });
+            graphData.links.push({ source: parent, target: key });
+            if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                addNodesAndLinks(key, obj[key], currentGroup);
+            } else if (Array.isArray(obj[key])) {
+                obj[key].forEach((subKey) => {
+                    graphData.nodes.push({ id: subKey, group: currentGroup });
+                    graphData.links.push({ source: key, target: subKey });
+                });
+            }
+        });
+    };
+
+    addNodesAndLinks('', technologies, 0);
+
+    // eslint-disable-next-line no-undef
+    const Graph = ForceGraph3D()(document.getElementById('graph'))
+        .graphData(graphData)
+        .nodeAutoColorBy('group')
+        .linkDirectionalParticles(2)
+        .linkDirectionalParticleSpeed(0.01)
+        .linkOpacity(0.2)
+        .linkWidth(1.5)
+        .nodeThreeObject((node) => {
+            const sprite = new SpriteText(node.id);
+            sprite.material.depthWrite = false; // make sprite background transparent
+            sprite.color = node.color;
+            sprite.textHeight = 16;
+            return sprite;
+        })
+        .backgroundColor('#f0f0f0')
+        .showNavInfo(false)
+        .linkAutoColorBy('type')
+        .linkColor(() => 'rgb(0, 0, 0)')
+        .width(360)
+        .height(310);
+
+    // Spread nodes a little wider
+    Graph.d3Force('charge').strength(-200);
+};
+
+onMounted(() => {
+    createGraph();
+});
+</script>
+
+<template>
+    <div class="col-span-12 lg:col-span-6 xl:col-span-4">
+        <Card>
+            <template #header>
+                <div class="flex justify-center text-xl mt-6">Technologies</div>
+            </template>
+            <template #content>
+                <div class="flex justify-center">
+                    <div id="graph"></div>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,10 +1,12 @@
 <script setup>
 import PresentationWidget from '@/components/dashboard/PresentationWidget.vue';
+import SkillsWidget from '@/components/dashboard/SkillsWidget.vue';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
         <PresentationWidget />
+        <SkillsWidget />
         <!-- <StatsWidget />
 
         <div class="col-span-12 xl:col-span-6">
