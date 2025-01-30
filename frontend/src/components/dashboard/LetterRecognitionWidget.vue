04c7129977bc206a4ec6afe9f81a33304d5f02e5
Pascal Pham <pham.anhkhoapascal@gmail.com>
2025-01-30 11:04:18 +0100

    frontend digit drawing widget

--- frontend/src/components/dashboard/LetterRecognitionWidget.vue
+++ frontend/src/components/dashboard/LetterRecognitionWidget.vue
@@ -1,34 +1,56 @@
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 import { VueSignaturePad } from 'vue-signature-pad';
 
-const signaturePad = ref(null);
-const recognizedLetter = ref('');
+const drawingPad = ref(null);
+const recognizedDigit = ref('');
+
+function dataURLtoBlob(dataURL) {
+    const arr = dataURL.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+}
 
 const sendDrawing = async () => {
-    const dataUrl = signaturePad.value.toDataURL();
-    const response = await fetch('YOUR_API_ENDPOINT', {
-        method: 'POST',
+    const { data } = drawingPad.value.saveSignature();
+    const blob = dataURLtoBlob(data);
+    const formData = new FormData();
+    formData.append('file', blob);
+
+    const response = await axios.post('http://localhost:8000/recognition/', formData, {
         headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ image: dataUrl })
+            'Content-Type': 'multipart/form-data'
+        }
     });
-    const result = await response.json();
-    recognizedLetter.value = result.letter;
+    recognizedDigit.value = response.data.digit;
+};
+
+const clearDrawing = () => {
+    drawingPad.value.clearSignature();
+    recognizedDigit.value = '';
 };
 </script>
 
 <template>
     <Card>
         <template #header>
-            <div class="flex justify-center text-xl mt-4 mx-4">Reconnaissance de lettres</div>
+            <div class="flex justify-center text-xl mt-4 mx-4">Reconnaissance de chiffre</div>
         </template>
         <template #content>
             <div class="flex flex-col items-center">
-                <VueSignaturePad width="175px" height="300px" ref="signaturePad" class="border" />
-                <button @click="sendDrawing" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Envoyer</button>
-                <div v-if="recognizedLetter" class="mt-4 text-xl">Lettre reconnue : {{ recognizedLetter }}</div>
+                <VueSignaturePad width="175px" height="175px" ref="drawingPad" class="border" />
+                <div class="flex gap-2">
+                    <button @click="sendDrawing" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Envoyer</button>
+                    <button @click="clearDrawing" class="mt-4 px-4 py-2 bg-red-500 text-white rounded">Effacer</button>
+                </div>
+                <div v-if="recognizedDigit != ''" class="mt-4 text-xl">Chiffre reconnu : {{ recognizedDigit }}</div>
             </div>
         </template>
     </Card>
